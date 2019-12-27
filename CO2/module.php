cb8c17210760b704ba4217fe27816a5a1287be86
Thorsten Zoerner <me@thorsten-zoerner.com>
2019-12-27 12:40:35 +0100

    staged

--- CO2/module.php
+++ CO2/module.php
@@ -5,6 +5,17 @@
 		{
 			//Never delete this line!
 			parent::Create();
+
+
+			// Register some required parameters for our Corrently CO2 Reading operation
+ 			$this->RegisterPropertyString("Postleitzahl", "69256");
+
+			$randstr = bin2hex(random_bytes(5));
+			$this->RegisterPropertyString("meterId", md5(time()."_".$randstr));
+
+			$secret = bin2hex(random_bytes(10));
+			$this->RegisterPropertyString("secret",$secret));
+
 		}
 
 		public function Destroy()
